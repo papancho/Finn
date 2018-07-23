1375085e34f646bec06f9b44dd65a2bbfb5d4df0
t-sawass <t-sawass@t-sawasss-MBP.guest.corp.microsoft.com>
2018-07-23 12:26:10 -0700

    Initial Commit

---
+++ Finn/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Finn
+//
+//  Created by t-sawass on 7/18/18.
+//  Copyright © 2018 t-sawass. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
