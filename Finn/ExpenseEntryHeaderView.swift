64ae3db733fe417cf7bfe81eae6f275557645456
t-sawass <t-sawass@t-sawasss-MBP.guest.corp.microsoft.com>
2018-07-24 17:20:14 -0700

    WIP

---
+++ Finn/ExpenseEntryHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ExpenseEntryHeaderView.swift
+//  Finn
+//
+//  Created by t-sawass on 7/24/18.
+//  Copyright © 2018 t-sawass. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseEntryHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Finn/ExpenseModel.swift
@@ -0,0 +1,9 @@
+//
+//  ExpenseModel.swift
+//  Finn
+//
+//  Created by t-sawass on 7/24/18.
+//  Copyright © 2018 t-sawass. All rights reserved.
+//
+
+import Foundation
